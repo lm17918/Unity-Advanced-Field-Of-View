24059a6d040e567a96ef8aaf0f47af772fdb9ca0
lorenzo <lorenzo.mirante@outlook.it>
2021-01-14 14:20:26 +0100

    update

--- README.md
+++ README.md
@@ -1,35 +1,25 @@
-# unity-Field-of-view-advanced
+# unity-2D-Field-of-view-advanced
 
-In this project I expand the project called "unity field of view"  adding several targets that much be seen from the main character.
-As you can see, in this image I use the same field ov view from the other project but in the map the are several capsule object that are marked as "targets".
-<img src="https://github.com/lm17918/unity-Field-of-view-advanced/blob/master/example%201.PNG" width="200"> <img src="https://github.com/lm17918/unity-Field-of-view-advanced/blob/master/example%202.PNG" width="185"> 
+In this repository I expand the project called "unity field of view" from https://www.youtube.com/watch?v=rQG9aUWarwE&ab_channel=SebastianLague.
+The purpose of this project is to give to the user a starting point to imprement a 2D simulation of what a point of interest would see in an environment.
 
-In this version some importations are printed on the screen:
-<img src="https://github.com/lm17918/unity-Field-of-view-advanced/blob/master/example%203.PNG" width="800">
-These info represent:
--The number of targets seen by the main character in the 360°
--The number of targets seen by the main character only in its field of view
--The number of targets hidden by obstacles in the field of viewof the main character  
+I use for the environment the "Sci-Fi Styled Modular Pack", a free package available on the Unity store. Here I place three 3DGameobjects capsule, two for the targets and one for the main character that is controlled by the user with mouse and keyboard (for a better explanation about how these controls are implemented look at https://www.youtube.com/watch?v=rQG9aUWarwE&ab_channel=SebastianLague).
+fig2
 
-Looking at the code in the "controller" script you will be able to see three functions called :
--FOV_seen (where the number of target seen in the filed of view is calculated)
--FOV_hidden (where the number of target hidden in the filed of view is calculated)
--FOV_360 (where the number of target seen at 360° in the filed of view is calculated)
+the controled character setup is cofigured from one script that is attached to him colled "Controller" where the user can set:
+the view radius: the max distance from the character center for which we assume the character is able to see.
+the view angle: the FOV angle from the character center.
+the Traget Mask: The Layer name to which are assigned the two other capsule Gameobject
+the Obstable Mast: the Layer assigned to the environment.
 
-In these functions there are some lines that are not executed by the program because are comment.
-In "FOV_seen" you can use those lines to have a list of the names of the targets seen in the field of view(targets_seen). 
-In "FOV_hidden" you can use those lines to have a list of the names of the targets hidden in the field of view(hidden_targets). 
-In "FOV_360" you can use those lines to have a list of the names of the targets see at 360°(degreespoints).
+In this environment the characted has a field of view that is coloured in green, that simulates what the characted can see, while the rest of the 360 degrees FOV around him is shown as a blue pattern ( what the character would see if it would turn around the 360 degrees). When the character is in front of an obstacles that do not allow him to see thru, the FOV until the obstable is presented as green pattern while the rest of the field of view that is blocked by the obstacle is shown as red.
 
-I hope that this code will help you with your project!
+When the project is running there are also two capsule moving in the environment. These two capsules have their layer mask set as "targets". when one of the capsule enter the view radius of the chatracter it changes its colour with the same FOV colour, so if it will be in the blue part of the view radius will become blue, if it will reach the green part will become green and if it will he hidden behind an obstable will get the red colour. Once it leaves the view radius of the main character it goes back to a white colur. this is just an example to make the player understand the position of the targets relative to the FOV of the main character.
 
+In the script called controller you can access to several lists representing:
+all the targets in the scene, accessing the List < GameObject > allTargets
+the targets seen in the FOV, accessing the List < GameObject > targetsInFOV
+the targets hidden by obstacles, accessing the List < GameObject > targetsHideen
+the targets outside the FOV but in the view radius, accessing the List < GameObject > targetsOutsideFOV
 
-For any question send me an email at lorenzo.mirante@outlook.it
-
-
-
-
-
-
-
-
+<img src="https://github.com/lm17918/unity-Field-of-view-advanced/blob/master/example%201.PNG" width="200">
---
+++ Unity project/Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,181 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FieldOfView: MonoBehaviour {
+  [HideInInspector]
+  //number of rays will be casted per degree 
+  private float meshResolution = 1;
+  private int edgeResolveIterations = 4;
+  private float edgeDstThreshold = 1;
+  public MeshFilter viewMeshFiltergreen;
+  public MeshFilter viewMeshFilterred;
+  public MeshFilter viewMeshFilterblue;
+  private Controller controller;
+  private Mesh viewMeshgreen;
+  private Mesh viewMeshred;
+  private Mesh viewMeshblue;
+  private int[] triangles;
+  private Vector3[] vertices;
+  void Start() {
+    controller = this.GetComponent < Controller > ();
+    viewMeshgreen = new Mesh();
+    viewMeshFiltergreen.mesh = viewMeshgreen;
+
+    viewMeshred = new Mesh();
+    viewMeshFilterred.mesh = viewMeshred;
+
+    viewMeshblue = new Mesh();
+    viewMeshFilterblue.mesh = viewMeshblue;
+
+  }
+
+  void LateUpdate() {
+    //show green FOV
+    DrawFieldOfView("green", viewMeshgreen);
+    //show blue FOV
+    DrawFieldOfView("blue", viewMeshblue);
+    // Show red FOV
+    DrawFieldOfView("red", viewMeshred);
+  }
+
+  public void DrawFieldOfView(string colorSpace, Mesh viewMesh) {
+    int stepCount;
+    float stepAngleSize;
+    if (colorSpace == "blue") {
+      stepCount = Mathf.RoundToInt((360 - controller.viewAngle) * meshResolution);
+      stepAngleSize = (360 - controller.viewAngle) / stepCount;
+    } else {
+      stepCount = Mathf.RoundToInt(controller.viewAngle * meshResolution);
+      stepAngleSize = controller.viewAngle / stepCount;
+    }
+
+    List < Vector3 > viewPoints = new List < Vector3 > ();
+    ViewCastInfo oldViewCast = new ViewCastInfo();
+    for (int i = 0; i <= stepCount; i++) {
+      float angle;
+      if (colorSpace == "blue") {
+        angle = transform.eulerAngles.y + controller.viewAngle / 2 + stepAngleSize * i;
+      } else {
+        angle = transform.eulerAngles.y - controller.viewAngle / 2 + stepAngleSize * i;
+      }
+      ViewCastInfo newViewCast = ViewCast(angle);
+
+      if (i > 0) {
+        bool edgeDstThresholdExceeded = Mathf.Abs(oldViewCast.dst - newViewCast.dst) > edgeDstThreshold;
+        if (oldViewCast.hit != newViewCast.hit || (oldViewCast.hit && newViewCast.hit && edgeDstThresholdExceeded)) {
+          EdgeInfo edge = FindEdge(oldViewCast, newViewCast);
+          if (edge.pointA != Vector3.zero) {
+            viewPoints.Add(edge.pointA);
+          }
+          if (edge.pointB != Vector3.zero) {
+            viewPoints.Add(edge.pointB);
+          }
+        }
+      }
+      viewPoints.Add(newViewCast.point);
+      oldViewCast = newViewCast;
+    }
+    int vertexCount = viewPoints.Count + 1;
+    if (colorSpace == "red") {
+      vertices = new Vector3[(vertexCount) * 2];
+      triangles = new int[(vertexCount) * 6];
+      for (int i = 0; i < vertexCount; i++) {
+        Vector3 vertex = transform.InverseTransformPoint(viewPoints[(i == viewPoints.Count) ? 0 : i]);
+        vertices[i * 2] = vertex;
+        vertices[i * 2 + 1] = vertex.normalized * controller.viewRadius;
+      }
+      for (int i = 0; i < (vertexCount); i++) {
+        int j = (vertexCount - 1 == i) ? 0 : i;
+        triangles[i * 6 + 0] = j * 2 + 0;
+        triangles[i * 6 + 1] = j * 2 + 1;
+        triangles[i * 6 + 2] = j * 2 + 2;
+        triangles[i * 6 + 3] = j * 2 + 1;
+        triangles[i * 6 + 4] = j * 2 + 3;
+        triangles[i * 6 + 5] = j * 2 + 2;
+      }
+    } else {
+      vertices = new Vector3[vertexCount];
+      triangles = new int[(vertexCount - 2) * 3];
+      vertices[0] = Vector3.zero;
+      for (int i = 0; i < vertexCount - 1; i++) {
+        vertices[i + 1] = transform.InverseTransformPoint(viewPoints[i]);
+        if (i < vertexCount - 2) {
+          triangles[i * 3] = 0;
+          triangles[i * 3 + 1] = i + 1;
+          triangles[i * 3 + 2] = i + 2;
+        }
+      }
+    }
+    viewMesh.Clear();
+    viewMesh.vertices = vertices;
+    viewMesh.triangles = triangles;
+    viewMesh.RecalculateNormals();
+  }
+
+  EdgeInfo FindEdge(ViewCastInfo minViewCast, ViewCastInfo maxViewCast) {
+    float minAngle = minViewCast.angle;
+    float maxAngle = maxViewCast.angle;
+    Vector3 minPoint = Vector3.zero;
+    Vector3 maxPoint = Vector3.zero;
+
+    for (int i = 0; i < edgeResolveIterations; i++) {
+      float angle = (minAngle + maxAngle) / 2;
+      ViewCastInfo newViewCast = ViewCast(angle);
+
+      bool edgeDstThresholdExceeded = Mathf.Abs(minViewCast.dst - newViewCast.dst) > edgeDstThreshold;
+      if (newViewCast.hit == minViewCast.hit && !edgeDstThresholdExceeded) {
+        minAngle = angle;
+        minPoint = newViewCast.point;
+      } else {
+        maxAngle = angle;
+        maxPoint = newViewCast.point;
+      }
+    }
+
+    return new EdgeInfo(minPoint, maxPoint);
+  }
+
+  ViewCastInfo ViewCast(float globalAngle) {
+    Vector3 dir = DirFromAngle(globalAngle, true);
+    RaycastHit hit;
+
+    if (Physics.Raycast(transform.position, dir, out hit, controller.viewRadius, controller.obstacleMask)) {
+      return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
+    } else {
+      return new ViewCastInfo(false, transform.position + dir * controller.viewRadius, controller.viewRadius, globalAngle);
+    }
+  }
+
+  public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal) {
+    if (!angleIsGlobal) {
+      angleInDegrees += transform.eulerAngles.y;
+    }
+    return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+  }
+
+  public struct ViewCastInfo {
+    public bool hit;
+    public Vector3 point;
+    public float dst;
+    public float angle;
+
+    public ViewCastInfo(bool _hit, Vector3 _point, float _dst, float _angle) {
+      hit = _hit;
+      point = _point;
+      dst = _dst;
+      angle = _angle;
+    }
+  }
+
+  public struct EdgeInfo {
+    public Vector3 pointA;
+    public Vector3 pointB;
+
+    public EdgeInfo(Vector3 _pointA, Vector3 _pointB) {
+      pointA = _pointA;
+      pointB = _pointB;
+    }
+  }
+
+}
